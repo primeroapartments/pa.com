746fe2653444fd6f092425e53c2779b75adc3fb8
Margreet Dijkstra <dijkstra.margreet@gmail.com>
2019-09-16 14:37:35 +0100

    markers visible

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -7,9 +7,12 @@ class ApartmentsController < ApplicationController
         lat: apartment.latitude,
         lng: apartment.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { apartment: apartment })
+        # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
       }
+
+
+    end
   end
-end
 
   def index
     @apartments = Apartment.all
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,56 +1,23 @@
-// import 'mapbox-gl/dist/mapbox-gl.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
 // import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+
 
 const mapElement = document.getElementById('map');
 
 if (mapElement) { // only build a map if there's a div#map to inject into
-  // const map = new mapboxgl.Map({
-  //   container: 'map',
-  //   style: 'mapbox://styles/mapbox/streets-v10'
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-  center: [-1.7603, 53.3781], // starting position [lng, lat]
-  zoom: 6, // starting zoom
+  mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
+  const map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-1.7603, 53.3781], // starting position [lng, lat]
+    zoom: 6, // starting zoom
+  });
+
+  const markers = JSON.parse(mapElement.dataset.markers);
+
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
   });
 }
-
-var geojson = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-77.032, 38.913]
-    },
-    properties: {
-      title: 'Mapbox',
-      description: 'Washington, D.C.'
-    }
-  },
-  {
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-122.414, 37.776]
-    },
-    properties: {
-      title: 'Mapbox',
-      description: 'San Francisco, California'
-    }
-  }]
-};
-
-// add markers to map
-geojson.features.forEach(function(marker) {
-
-  // create a HTML element for each feature
-  var el = document.createElement('div');
-  el.className = 'marker';
-
-  // make a marker for each feature and add to the map
-  new mapboxgl.Marker(el)
-    .setLngLat(marker.geometry.coordinates)
-    .addTo(map);
-});
