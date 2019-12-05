b2f81c169f59319a2afd9e1761df2e0a388735c6
Margreet Dijkstra <dijkstra.margreet@gmail.com>
2019-12-05 13:34:09 +0000

    Deleted all references to Preston, deleted routing to API folder that was previously removed

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   get 'cardiff', to: 'apartments#cardiff'
   get 'basingstoke', to: 'apartments#basingstoke'
   get 'birmingham', to: 'apartments#birmingham'
-  get 'preston', to: 'apartments#preston'
 
   get 'apartments', to: 'apartments#index'
   get 'apartments/new', to: 'apartments#new'
@@ -28,11 +27,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
-
-  namespace :api, defaults: { format: :json } do
-      namespace :v1 do
-        resources :apartments, only: [ :index ]
-    end
-  end
 end
 
